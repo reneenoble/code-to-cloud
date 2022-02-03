ffd30a23852e567769a77711ca0536031f1d032c
Renee Noble <renee.noble@hotmail.com>
2022-02-03 17:51:35 +1100

    query string for different styles

--- server.py
+++ server.py
@@ -4,13 +4,19 @@ invite_app = Flask('app')
 
 @invite_app.route("/view")
 def view_invite():
-    to = "Sarah"
+    to = "Tom"
     event = "Artermis' birthday party"
     date = "February 10th"
     time = "4am"
     sender = "Jack"
 
-    template = "invite-kids.html"
+    # http://192.168.1.122:8080/view?style=kids
+    # http://192.168.1.122:8080/view?event=Bibi's+Bday&to=Clair&date=Monday+32nd+of+Mocktober&time=4pm&sender=Renee&style=cat
+    style = request.args.get('style')
+
+    template = "invite-" + style + ".html"
+    
+
     return render_template(template, to=to, event_name=event, date=date, time=time, sender=sender)
 
 if __name__ == '__main__':
