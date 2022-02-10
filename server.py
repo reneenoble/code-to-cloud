d4d3e00e87cb2c12d0d3d564e8d664a21499e443
Renee Noble <renee.noble@hotmail.com>
2022-02-10 17:30:54 +1100

    added form page

--- server.py
+++ server.py
@@ -1,18 +1,30 @@
 from flask import Flask, request, render_template
 
-invite_app = Flask('app')
+app = Flask('app')
 
-@invite_app.route("/view")
+@app.route("/")
+def form():
+    return render_template("form.html")
+
+
+@app.route("/view")
 def view_invite():
-    to = "Tom"
-    event = "Artermis' birthday party"
-    date = "February 10th"
-    time = "4am"
-    sender = "Jack"
+    # to = "Sarah"
+    # event = "Artemis' birthday party"
+    # date = "February 10th"
+    # time = "4am"
+    # sender = "Jack"
+
+    to = request.args.get("to")
+    event = request.args.get("event")
+    date = request.args.get("date")
+    time = request.args.get("time")
+    sender = request.args.get("sender")
+    style = request.args.get('style')
 
     # http://192.168.1.122:8080/view?style=kids
     # http://192.168.1.122:8080/view?event=Bibi's+Bday&to=Clair&date=Monday+32nd+of+Mocktober&time=4pm&sender=Renee&style=cat
-    style = request.args.get('style')
+    
 
     template = "invite-" + style + ".html"
     
@@ -20,4 +32,4 @@ def view_invite():
     return render_template(template, to=to, event_name=event, date=date, time=time, sender=sender)
 
 if __name__ == '__main__':
-    invite_app.run(debug=True, use_reloader=True, host='0.0.0.0', port=8080)
+    app.run(debug=True, use_reloader=True, host='0.0.0.0', port=8080)
