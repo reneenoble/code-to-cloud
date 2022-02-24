dbd209347106a49c25532c1034f3245d1d8ff192
reneenoble <renee.noble@hotmail.com>
2022-02-24 18:00:51 +1100

    azure blob

--- server.py
+++ server.py
@@ -1,24 +1,30 @@
 from sqlite3 import connect
 from flask import Flask, request, render_template
 import json, os
-# import dotenv
-# from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
+import dotenv
+from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
+from azure.core.exceptions import ResourceExistsError, ResourceNotFoundError
 
 app = Flask('app')
 
 local_path = "./data"
 
-# from dotenv import load_dotenv
-# load_dotenv(".env")
+from dotenv import load_dotenv
+load_dotenv(".env")
 
 
-# # Make Containter 
-# connect_str = os.getenv('AZURE_STORAGE_CONNECTION_STRING')
-# blob_service_client = BlobServiceClient.from_connection_string(connect_str)
+# Make Containter 
+connect_str = os.getenv('AZURE_STORAGE_CONNECTION_STRING')
+blob_service_client = BlobServiceClient.from_connection_string(connect_str)
 
-# # Create a unique name for the container
-# container_name = "rsvps"
-# container_client = blob_service_client.create_container(container_name)
+# Create a unique name for the container
+container_name = "rsvps-live"
+
+try:
+    container_client = blob_service_client.create_container(container_name)
+except ResourceExistsError:
+    container_client = blob_service_client.get_container_client(container_name)
+
 
 
 @app.route("/")
@@ -52,6 +58,22 @@ def view_invite():
 
     return render_template(template, to=to, event_name=event, date=date, time=time, sender=sender, eventId=eventId)
 
+@app.route("/events")
+def events():
+    blob_list = container_client.list_blobs()
+
+    blob_list = [b.name for b in blob_list]
+
+    return render_template("events.html", event_list=blob_list)
+
+@app.route("/event-rsvps/<event_file>")
+def event_rsvps(event_file):
+    blob_client = blob_service_client.get_blob_client(container=container_name, blob=event_file)
+    attendees = str(blob_client.download_blob().readall(), "utf-8")
+    attendees = attendees.split("\n")
+    print(attendees)
+    return render_template("event-rsvps.html", attendees=attendees)
+
 
 @app.route("/rsvp", methods=('GET', 'POST'))
 def rsvp():
@@ -61,14 +83,53 @@ def rsvp():
     event_ID = event_data[1]
     filename = event_ID + ".txt"
 
-    filepath = os.path.join(local_path, filename)
+    # filepath = os.path.join(local_path, filename)
 
-    with open(filepath ,"w") as f:
-        f.write(attendee)
+    
+    try:
+        sync_blob(event_ID, attendee)
+        return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
+    except:
+        return json.dumps({'success':True}), 400, {'ContentType':'application/json'}
 
-    return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
 
+def sync_blob(event_ID, attendee):
+    filename = event_ID + ".txt"
+    local_file = os.path.join(local_path, filename)
 
+    blob_client = blob_service_client.get_blob_client(container=container_name, blob=filename)
+
+    try:
+        print("\nDownloading blob to \n\t" + local_file)
+
+        # Get the data out of the file, split it into a list of people who have already RSVPed
+        attendees = str(blob_client.download_blob().readall(), "utf-8")
+        attendees = attendees.split("\n")
+
+        if attendee not in attendees:
+            attendees.append(attendee)
+        
+            # Make the local file
+            with open(local_file, "w") as download_file:
+                download_file.writelines("\n".join(attendees))
+
+            # Push the local file to Azure blob
+            with open(local_file, "rb") as data:
+                blob_client.delete_blob()
+                blob_client.upload_blob(data)
+
+    # The blob wasn't there, oh no!
+    except ResourceNotFoundError:
+        # If the file doesn't exist, make a local file, conatining the single attendee from the request
+        with open(local_file, "w") as f:
+            f.write(attendee)
+
+        # Write the file to Azure blob
+        with open(local_file, "rb") as data:
+            blob_client.upload_blob(data)
+
+    
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True, host='0.0.0.0', port=8080)
+    # sync_blob("Tina|bday party", "Renee")
